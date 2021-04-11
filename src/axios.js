60746909c77a23b696cef7c24a7c0d698385a511
RIAD Houssame <riad.houssame.eleve@ehtp.ac.ma>
2021-04-11 08:59:25 +0000

    last commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://tinder-backend-ff33.herokuapp.com',
 });
 
 export default instance;
